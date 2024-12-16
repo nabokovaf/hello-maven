e684951eb79f6a64a225815ffdb1157d0f72e699
Alex Nabokov <thesums@yandex.ru>
2024-12-16 17:28:13 +0400

    Коммит добавения теста CalculatorTest_sprint3

--- src/main/java/ru/yandex/praktikum/Calculator.java
+++ src/main/java/ru/yandex/praktikum/Calculator.java
@@ -1,6 +1,22 @@
 package ru.yandex.praktikum;
 
 public class Calculator {
+    final static int MIN_AMOUNT = 300;
+
+    public static int calculate(Integer distance, Boolean isLarge, Boolean isFragile) {
+        int currentAmount = 0;
+
+        if (distance > 10) currentAmount += 200;
+        else if (distance > 2) currentAmount += 100;
+        else currentAmount += 50;
+
+        if (isLarge) currentAmount += 200;
+        else currentAmount += 100;
+
+        if (isFragile) currentAmount += 300;
+
+        return Math.max(currentAmount, MIN_AMOUNT);
+    }
 
     public int sum(int a, int b) {
         return a + b;
